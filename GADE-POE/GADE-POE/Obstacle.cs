40cbc6e459dc9a7cb62aab0174532b6fe5e991d5
BillyLeWizardo <mr.williammhughes@gmail.com>
2022-11-17 15:36:45 +0200

    Added in Obstacle + EmptyTile

---
+++ GADE-POE/GADE-POE/Obstacle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE_POE
+{
+    public class Obstacle : Tile
+    {
+        public Obstacle(int x, int y) : base(x, y)
+        {
+            Type = TileType.Obstacle;
+        }
+    }
+}
