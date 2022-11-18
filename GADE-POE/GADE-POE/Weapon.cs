6216fd035b61be7ecfc21a4b729ebaa03790f965
OmegaOckie <ockiejnr@gmail.com>
2022-11-18 04:01:59 +0200

    Q.2.1
    Weapon abstract class

---
+++ GADE-POE/GADE-POE/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE_POE
+{
+    abstract class Weapon : Item
+    {
+        protected int weaponDamage;
+        public int WeaponDamage { get { return weaponDamage; } set { weaponDamage = value; } }
+
+        protected int weaponRange;
+        public virtual int WeaponRange { get { return weaponRange; } set { weaponRange = value; } }
+
+        protected int weaponDurability;
+        public int WeaponDurability { get { return weaponDurability; } set { WeaponDurability = value; } }
+
+        protected int weaponCost;
+        public virtual int WeaponCost { get { return weaponCost; } set { weaponCost = value; } }
+
+        protected enum weaponType
+        {
+            MELEE,
+            RANGED
+        }
+
+        public Weapon(int weaponX, int weaponY) : base( weaponX, weaponY)
+        {
+
+        }
+    }
+}
